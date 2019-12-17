24c62d480d8acd3dc31489ade20f285a6e78e374
Breno Lopes <breno.souza.developer@gmail.com>
2019-12-16 21:33:18 -0300

    adicionando filtro de eventos para usuario logado

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,16 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./home.css";
 import { Navbar, Card } from "../../components";
 import { useSelector } from "react-redux";
 
 import firebase from "../../configs/firebase";
-export default function Home({match}) {
-  const [eventos, setEventos] = useState(['']);
-  const [pesquisa, setPesquisa] = useState('');
+export default function Home({ match }) {
+  const [eventos, setEventos] = useState([""]);
+  const [pesquisa, setPesquisa] = useState("");
   const usuarioEmail = useSelector(state => state.usuarioEmail);
 
-  let listaEventos = []
-  
+  let listaEventos = [];
+
   useEffect(() => {
     if (match.params.parametro) {
       firebase
@@ -49,25 +49,30 @@ export default function Home({match}) {
         });
     }
   });
-  
+
   return (
     <>
       <Navbar />
       <div className="row p-3 ">
-            <h2 className="mx-auto p-5">Eventos Publicados</h2>
-            <input onChange={(e) => setPesquisa(e.target.value)} type="text" className="form-control text-center" placeholder="Pesquisar evento pelo título..." />
-        </div>
+        <h2 className="mx-auto p-5">Eventos Publicados</h2>
+        <input
+          onChange={e => setPesquisa(e.target.value)}
+          type="text"
+          className="form-control text-center"
+          placeholder="Pesquisar evento pelo título..."
+        />
+      </div>
       <div className="row mt-5 p-5">
-        {
-          eventos.map(item => <Card 
+        {eventos.map(item => (
+          <Card
             key={item.id}
             id={item.id}
             img={item.foto}
             titulo={item.titulo}
             detalhes={item.detalhes}
             visualizacoes={item.visualizacoes}
-          />)
-        }
+          />
+        ))}
       </div>
     </>
   );
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ export default function Routes() {
 			<React.Fragment>
 				<Switch>
 					<Route exact path="/" component={Home} />
+					<Route path="/eventos/:parametro" component={Home} />
 					<Route path="/cadastrar-usuario" component={CadastroUsuario} />
 					<Route path="/login" component={Login} />
 					<Route path="/recuperar-senha" component={RecuperarSenha} />
